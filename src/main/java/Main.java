6ea0158e43c3cd364f60a38db1bd85cfc497d659
Jorden Ross <JRoss9117@gmail.com>
2024-03-06 08:57:43 -0500

    ive made changes:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,40 @@ import java.util.Scanner;
 
 public class Main {
 
-   Int userInput;   //Methods to be called
-    Int randomNumber;
-
     public static void main(String[] args) {
         //Methods: userInput() , randomNumber();
         // If userInput() > randomNumber() return "Too large";
         //else if userInput() < randomNumber() return "Too small";
         // else return "Correct guess!!";
-
+userInput();
+randomNumber();
+        random number = 10
     }
+
     public static String userInput() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your guess. ");
 
-        int n = scan.nextInt;
+        int userInput = scan.nextInt(); //reads user input represented by n
+
+        if (userInput > randomNumber) {
+            System.out.println("Too large");
+
+        } else if (userInput < randomNumber) {
+            System.out.println("Too small");
+        } else {
+            System.out.println("Correct guess!!");
+
+        }
+        return String.valueOf(userInput);
+
+
+    } return userInput;
+}
+        public static int randomNumber(){ //
+
+
+
 
 
 
@@ -29,3 +48,4 @@ public class Main {
 
     }
 }
+
