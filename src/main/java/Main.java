75857b749838d323dd6072ff5d4dc4fb7578e7a2
Jorden Ross <JRoss9117@gmail.com>
2024-03-06 08:26:40 -0500

    Ive made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+   Int userInput;   //Methods to be called
+    Int randomNumber;
+
+    public static void main(String[] args) {
+        //Methods: userInput() , randomNumber();
+        // If userInput() > randomNumber() return "Too large";
+        //else if userInput() < randomNumber() return "Too small";
+        // else return "Correct guess!!";
+
+    }
+    public static String userInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your guess. ");
+
+        int n = scan.nextInt;
+
+
+
+
+
     }
 }
